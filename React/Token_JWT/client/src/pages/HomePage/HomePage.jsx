99b0a86073814bbce8808c6b22aee55e87229ddb
NikotoKoto <floris.nicolas77@gmail.com>
2025-02-12 16:11:00 +0100

    ✨  TJ_01: Create Homepage and SignUp component

---
+++ React/Token_JWT/client/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export default function HomePage() {
+  return (
+    <HomePageStyled>HomePage</HomePageStyled>
+  )
+}
+const HomePageStyled = styled.div`
+    
+`
---
+++ React/Token_JWT/client/src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,12 @@
+
+import styled from 'styled-components'
+
+export default function SignUp() {
+  return (
+    <SignUpStyled>SignUp</SignUpStyled>
+  )
+}
+
+const SignUpStyled =styled.div`
+    
+`
